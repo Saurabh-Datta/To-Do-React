a64a48952571a6c6056b97e6103b1e2751d1514e
Saurabh-Datta <saurabhdatta222001@gmail.com>
2021-12-14 21:50:03 +0530

    Added Dark and Light Theme to the App

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Header from "./Header";
 import Input from "./Input";
+import SwitchButton from './ThemeButton';
+import React, { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
 
 function App() {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div className="App">
+    <div className={`App bg ${darkMode ? "bg-dark" : "bg-light"}`} style={{height:"98vh"}}>
       <Header />
+      <SwitchButton />
       <Input />
     </div>
   );
--- to-do/src/Input.js
+++ to-do/src/Input.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { useState } from 'react'
+import { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
+
+
 
 function Input() {
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
     const [task,setTask] = useState("")
     const [tasks,setTasks] = useState(["try creating a task","the tasks will be shown here"])
     const onClick=()=>{
@@ -11,10 +17,10 @@ function Input() {
     }
     return (
         <div>
-            <input id="task" type="text" placeholder="Enter the Task here" value={task} onChange={e=>{setTask(e.target.value)
-            }}></input>
-            <button onClick={onClick}>Add</button>
-            <ul>
+            <input id="task" type="text" placeholder="Enter the Task here" style={{height:"2em"}} value={task} onChange={e=>{setTask(e.target.value)
+            }} className={`input ${darkMode ? "input-dark" : "input-light"}`}></input>
+            <button style={{"margin":"10px",borderRadius:"10%",border:"none",height:"2em"}} onClick={onClick} className={`btn ${darkMode ? "btn-dark" : "btn-light"}`}>Add</button>
+            <ul style={{marginLeft:"auto",marginRight:"auto",textAlign:"left",display:"table"}} className={`ul ${darkMode ? "ul-dark" : "ul-light"}`}>
                 {tasks.map((t,index)=>{
                     return <li key={index}>{t}</li>
                 })}
